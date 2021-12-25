59d11d6cee7ee9044201d627dd03ea1b3b3e0d7f
Nissrine Canina <niss.canina@gmail.com>
2021-12-24 20:13:58 -0500

    commit tree 6dcfdb44cbdfbeb4233bf23b8773630a52d65ccb

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 3, "M", "brown", "nice", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 6, "M", "black", "fierce", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "F", "white", "lazy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 1, "M", "dark brown", "cute", 1);
+INSERT INTO bears VALUES (5, "Melissa", 6, "F", "light brown", "funy", 1);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "black", "nice", 1);
+INSERT INTO bears VALUES (7, "Wendy", 8, "F", "beige", "strong", 1);
+INSERT INTO bears VALUES (8, null, 2, "M", "white", "playful", 1);
+
